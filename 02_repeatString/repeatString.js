3d050ac5ef71220c2082e183f19be99cfa260cee
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-03 01:51:39 +0100

    finished ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, num) {
+    outputString = ''
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            outputString += givenString;
+        }
+    } else {
+        outputString = 'ERROR';
+    }
+    return outputString;
 };
 
 // Do not edit below this line
