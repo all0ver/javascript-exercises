1982110f0b05a8f2d4d867b773ec18e38129e71d
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-03 17:23:57 +0100

    finished ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2< 0 || typeof(int1) != 'number' || typeof(int2) != 'number') {
+        return 'ERROR';
+    }
+    if (int1 > int2) {
+        let help = int1;
+        int1 = int2;
+        int2 = help;
+    }
+    let sum = 0;
+    for (let i = int1; i <= int2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
