648417d95dc420e3aff65924732b6c41b35aee6c
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-03 17:39:53 +0100

    finished ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenTemp) {
+  let convereted = parseFloat((givenTemp-32)*5/9).toFixed(1);
+  return parseFloat(convereted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenTemp) {
+  let convereted = parseFloat((givenTemp*1.8)+32).toFixed(1);
+  return parseFloat(convereted);
 };
 
 // Do not edit below this line
