9ee42615ee3fb830365e00b8ab79d2a6f6b2e1aa
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-10 00:49:17 +0100

    finished ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+  array.forEach(element => {
+    sum += element;
+  });
+  return parseInt(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+	for(let i = 1; i <=a; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
