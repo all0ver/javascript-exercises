d637878f41c943d6739a39775dc755985608833a
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-10 01:04:56 +0100

    finished ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s\']|_/g, "");
+    string = string.replace(/\s+/g, "");
+    string = string.toLowerCase();
+    Array.from(string);
+    for (let i = 0; i<string.length; i++) {
+        if (!(string[i] === string[string.length-i-1])) {
+            console.log("string: "+string[i]+" ;reversed: "+string[string.length-i-1]);
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
