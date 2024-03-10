864af265523b934795488444a8e495269efb6f78
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-10 01:13:43 +0100

    finished ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (!(typeof num == 'num')) {
+        num = parseInt(num);
+    }
+    let fib = 0;
+    let num2 = 1;
+    let help = 0
+    for (let i = 0; i < num; i++) {
+        help = fib;
+        fib += num2;
+        num2 = help;
+    }
+    return fib;
 };
 
 // Do not edit below this line
